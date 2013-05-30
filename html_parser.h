616164efdbaa002e8136a1fe51544797ce2668b1
Joe <joe.gain@gmail.com>
2013-05-30 17:21:06 +0200

    auto generated top header

--- html_parser.h
+++ html_parser.h
@@ -5,20 +5,27 @@
 extern "C" {
 #endif
 
+#include "include/html_parser_arith.h"
 #include "include/html_parser_assert.h"
 #include "include/html_parser_atom.h"
 #include "include/html_parser_attribute_list.h"
 #include "include/html_parser_attribute_reader.h"
 #include "include/html_parser_attribute_rep.h"
+#include "include/html_parser_document_builder.h"
 #include "include/html_parser_document_node.h"
+#include "include/html_parser_document_search.h"
 #include "include/html_parser_document_stack.h"
 #include "include/html_parser_document_tree.h"
 #include "include/html_parser_except.h"
+#include "include/html_parser_file_reader.h"
 #include "include/html_parser_fmt.h"
 #include "include/html_parser_mem.h"
+#include "include/html_parser_set.h"
+#include "include/html_parser_tag_lookup.h"
 #include "include/html_parser_tag_reader.h"
 #include "include/html_parser_tester.h"
 #include "include/html_parser_text.h"
+#include "include/html_parser_types.h"
 
 #ifdef __cplusplus
 }
